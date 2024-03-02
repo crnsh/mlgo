b3e9b208364d2a2f6caab76159af193d34b22e6c
Karan Handa <karan.handa.fox@gmail.com>
2024-03-02 17:35:59 +0530

    feat: add Div test and update Sqrt test

--- ml/ml_test.go
+++ ml/ml_test.go
@@ -27,10 +27,37 @@ func TestSqrt(t *testing.T) {
 	for i := 0; i < len(out.Data); i++ {
 		x_val := float64(x.Data[i])
 		out_val := float64(out.Data[i])
-		x_val_sqrt := math.Sqrt(float64(x.Data[i]))
+		exp_val := math.Sqrt(x_val)
 
-		if !floatEquals(math.Sqrt(x_val), out_val, eps) {
-			t.Errorf("Sqrt(%v) = %v, want %v", x_val, out_val, x_val_sqrt)
+		if !floatEquals(exp_val, out_val, eps) {
+			t.Errorf("SQRT(%v) = %v, Expected = %v", x_val, out_val, exp_val)
+		}
+	}
+}
+
+func TestDiv(t *testing.T) {
+
+	graph := Graph{ThreadsCount: 1}
+
+	a_data := []float32{1523, 12524, 1823, 142.324, 91238.233}
+	b_data := []float32{10.42, 12.44, 65.33, 5, 8.99}
+
+	a := NewTensor1DWithData(nil, TYPE_F32, 5, a_data)
+	b := NewTensor1DWithData(nil, TYPE_F32, 5, b_data)
+	out := Div(nil, a, b)
+
+	// run the computation
+	BuildForwardExpand(&graph, out)
+	GraphCompute(nil, &graph)
+
+	for i := 0; i < len(out.Data); i++ {
+		a_val := float64(a.Data[i])
+		b_val := float64(b.Data[i])
+		out_val := float64(out.Data[i])
+		exp_val := a_val / b_val
+
+		if !floatEquals(out_val, exp_val, eps) {
+			t.Errorf("DIV(%v, %v) = %v, Expected = %v", a_val, b_val, out_val, exp_val)
 		}
 	}
 }
